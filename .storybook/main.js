5659fcb557f3d9868451fde98bb347325e6a9eab
soaresdiogo <diogoschsoares@gmail.com>
2023-03-09 10:08:27 +0000

    chore: upgrade dependencies
    I updated the versions of vite and storybook.
    Fixed some dependencies to running correctly with new lib versions.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
-	stories: ['../packages/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-	framework: '@storybook/react',
-	core: {
-		builder: 'storybook-builder-vite',
-	},
-};
+  stories: ["../packages/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+}
